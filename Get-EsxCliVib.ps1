4805cb2bbde25436ad722c91b76a7cc309769958
Kevin Kirkpatrick <kevin@pinelabs.co>
2014-10-03 15:17:02 -0400

    Created

---
+++ Get-EsxCliVib.ps1
@@ -0,0 +1,124 @@
+﻿<#
+.SYNOPSIS
+	Get details on a specific ESXi VIB
+.DESCRIPTION
+	Get details on a specific ESXi VIB.
+
+	This can be usefule if you are looking for version information about a given package on an ESXi host.
+
+	This script/function is best utilized when sending a host object to it via the pipeline; see examples for more detail.
+.INPUT VMware.VimAutomation.ViCore.Impl.V1.Inventory.VMHostImpl
+.PARAMETER VMHost
+	Name of ESXi host
+.PARAMETER VIBName
+	Name of vib package you wish to query detail for
+.EXAMPLE
+	PS C:\> Get-VMHost | .\Get-EsxCliVib -VIBName hp-ams
+.EXAMPLE
+	PS C:\> Get-VMHost esxihost01.company.com | .\Get-EsxCliVib -VIBName hp-ams
+
+Host             : esxihost01.company.com
+Name             : hp-ams
+Version          : 550.10.0.1-07.1198610
+Vendor           : Hewlett-Packard
+InstallDate      : 2014-10-03
+ID               : Hewlett-Packard_bootbank_hp-ams_550.10.0.1-07.1198610
+CreationDate     : 2014-09-09
+Status           :
+HostVersion      : 5.5.0
+HostVersionBuild : 1892794
+HostMfg          : HP
+HostModel        : ProLiant BL460c Gen8
+
+.NOTES
+	20141002	K. Kirkpatrick		[+] Created
+	20141003	K. Kirkpatrick		[+] Added CBH (comment based help)
+#>
+
+[CmdletBinding(DefaultParameterSetName = 'Default',
+			   PositionalBinding = $true)]
+param
+(
+	[Parameter(ValueFromPipeline = $true,
+			   ValueFromPipelineByPropertyName = $true,
+			   Position = 0)]
+	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VMHostImpl]$VMHost,
+
+	[Parameter(Mandatory = $true,
+			   ValueFromPipeline = $false,
+			   ValueFromPipelineByPropertyName = $false,
+			   Position = 1)]
+	[string]$VIBName
+)
+
+Begin
+{
+	#Requires -Version 3
+	#Requires -PSSnapin VMware.VimAutomation.Core
+
+		# clear/set final $result variable
+	$result = @()
+}# begin
+
+Process
+{
+
+	try
+	{
+		Write-Verbose -Message "Working on $($_.Name)..."
+			# Clear/set variables
+		$colHpVib = @()
+		$esxcli = $null
+		$softwareList = $null
+			# Assign vmhost query objects to a variable to be called later
+		$esxcli = Get-EsxCli -VMHost $VMHost
+		$softwareList = $esxcli.software.vib.list() | Where-Object { $_.Name -eq 'hp-ams' }
+			# get uptime details
+		$bootTime = ($VMHost | Get-View).runtime.boottime
+		$calcUptime = ((Get-Date) - $bootTime)
+			# Create custom object to store host/vib information - use 'automatic foreach' notation (.) to call the detail for each property
+		$objHpVib = New-Object -TypeName psobject -Property @{
+			Host = $_.Name
+			VibName = $softwareList.Name
+			VibVersion = $softwareList.Version
+			VibVendor = $softwareList.Vendor
+			VibInstallDate = $softwareList.InstallDate
+			VibID = $softwareList.ID
+			VibCreationDate = $softwareList.CreationDate
+			VibStatus = $softwareList.Status
+			HostUptimeDays = $calcUptime.Days
+			HostVersion = $_.Version
+			HostVersionBuild = $_.Build
+			HostMfg = $_.Manufacturer
+			HostModel = $_.Model
+		}# $objHpVib
+
+			# Assign current object data to temporary collection array variable
+		$colHpVib += $objHpVib
+			# Send collection detail for current object to the final $result variable
+		$result += $colHpVib
+	} catch
+	{
+			# Write a warning to the console if errors are encountered
+		Write-Warning -Message "Error gathering detail from $VMHost"
+			# clear/set the $colError array
+		$colError = @()
+			# create new object to store error detail
+		$objError = New-Object -TypeName psobject -Property @{
+			Host = $_.Name
+			Status = "$_"
+		}# $objError
+
+			# assign current object data to temp. error collection array variable
+		$colError += $objError
+			# send collection detail to final $result variable
+		$result += $colError
+
+	}# try/catch
+}# process
+
+End
+{
+		# call final result and define a custom order - output from the console can be formatted using regular export cmdlets (Export-Csv; Out-Gridview; Format-Table; etc.)
+	$result | Select-Object Host, VibName, VibVersion, VibVendor, VibInstallDate, VibID, VibCreationDate, VibStatus, HostUptimeDays, HostVersion, HostVersionBuild, HostMfg, HostModel
+}# end
